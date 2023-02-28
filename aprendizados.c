57a8c52a08e6d2b62a69e3cb3d2f841449529e64
FilhoDoan <doangalvaofilho@gmail.com>
2023-02-28 00:03:36 -0300

    Aprendizados primarios

---
+++ aprendizados.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+
+int main()
+{
+
+   // tipos do C  int, double, float, char
+
+   // Estruturas basicas de C
+
+   /* prints
+    puts --> string
+    int --> printf("%d", numero)
+    double --> printf("%lf", double)
+    char --> printf("%c", char)
+    string --> sprintf()
+*/
+   //-------------------------------------------------
+   // printando numeros
+   /*
+      printf("%d\n", 10);
+      printf("%lf\n", 10.0);
+      // char * --> sinalizar que tem mais de um char
+
+      char *n;
+      sprintf(n, "Doan");
+      printf("%s\n", n);
+   */
+   //-----------------------------------------------
+   /*
+   int pn;
+   int sn;
+
+   printf("Digite o primeiro numero:\n");
+
+   scanf("%d", &pn);
+
+   printf("Digite o segundo numero:\n");
+
+   scanf("%d", &sn);
+
+   int resultado = pn + sn;
+
+   printf("%d\n", resultado);
+   */
+
+   //-------------------------------------------------
+   // variavel cm char
+   /* 
+      char letra = 'D';
+      printf("%c\n", letra);
+
+      // string com varias letras
+
+      char letras[5] = {'D','O','A','N', '\0'};
+
+      printf("%s", letras);
+   */
+   // string com ponteiro e variaveis 
+
+/*
+   char *p;
+   sprintf(p, "Doan"); 
+   printf("%s", p);
+
+*/
+
+  /* 
+   char s[5] = "Doan\0";
+   printf("%s", s);
+ */
+//-----------------------------------------------------------------------------
+   //Estruturas condicionais 
+
+
+
+
+
+
+
+
+
+
+}
