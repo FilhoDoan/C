5b2e01ff75fe5abb4f10bd30e7953938892d1bf9
FilhoDoan <doangalvaofilho@gmail.com>
2023-02-28 23:28:43 -0300

    Arquivo do JavaScript

--- aprendizados.c
+++ aprendizados.c
@@ -7,77 +7,69 @@ int main()
 
    // Estruturas basicas de C
 
-   /* prints
-    puts --> string
-    int --> printf("%d", numero)
-    double --> printf("%lf", double)
-    char --> printf("%c", char)
-    string --> sprintf()
-*/
+   /*
+     prints
+     puts --> string
+     int --> printf("%d", numero)
+     double --> printf("%lf", double)
+     char --> printf("%c", char)
+     string --> sprintf()
+ */
    //-------------------------------------------------
    // printando numeros
    /*
       printf("%d\n", 10);
-      printf("%lf\n", 10.0);
-      // char * --> sinalizar que tem mais de um char
+      printf("%lf\n", 10.0); */
+   // char * --> sinalizar que tem mais de um char
 
-      char *n;
-      sprintf(n, "Doan");
-      printf("%s\n", n);
-   */
+   /*  char *n;
+    sprintf(n, "Doan");
+    printf("%s\n", n); */
+
    //-----------------------------------------------
-   /*
-   int pn;
-   int sn;
 
-   printf("Digite o primeiro numero:\n");
+   /*   int pn;
+     int sn;
 
-   scanf("%d", &pn);
+     printf("Digite o primeiro numero:\n");
 
-   printf("Digite o segundo numero:\n");
+     scanf("%d", &pn);
 
-   scanf("%d", &sn);
+     printf("Digite o segundo numero:\n");
 
-   int resultado = pn + sn;
+     scanf("%d", &sn);
 
-   printf("%d\n", resultado);
-   */
+     int resultado = pn + sn;
+
+     printf("%d\n", resultado);
+     */
 
    //-------------------------------------------------
    // variavel cm char
-   /* 
-      char letra = 'D';
-      printf("%c\n", letra);
 
-      // string com varias letras
+   /*  char letra = 'D';
 
-      char letras[5] = {'D','O','A','N', '\0'};
+    printf("%c\n",letra);
 
-      printf("%s", letras);
+    // string com varias letras
+
+    char letras[5] = {'D','O','A','N', '\0'};
+
+    printf("%s", letras); */
+
+   // string com ponteiro e variaveis
+
+   /*
+      char *p;
+      sprintf(p, "Doan");
+      printf("%s", p);
+
    */
-   // string com ponteiro e variaveis 
 
-/*
-   char *p;
-   sprintf(p, "Doan"); 
-   printf("%s", p);
-
-*/
-
-  /* 
-   char s[5] = "Doan\0";
-   printf("%s", s);
- */
-//-----------------------------------------------------------------------------
-   //Estruturas condicionais 
-
-
-
-
-
-
-
-
-
-
+   /*
+    char s[5] = "Doan\0";
+    printf("%s", s);
+  */
+   //-----------------------------------------------------------------------------
+   // Estruturas condicionais
 }
