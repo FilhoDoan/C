749ecdce4979711d57a473139c344005807060b4
FilhoDoan <doangalvaofilho@gmail.com>
2023-04-19 17:25:15 -0300

    Atualizando arquivos

---
+++ modulo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int num, resultado; 
+
+    printf("Digite seu numero \n");
+
+    scanf("%d", &num);
+
+    resultado = abs(num);
+
+    printf("%d", resultado);
+
+
+}
